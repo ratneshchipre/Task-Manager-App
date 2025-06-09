fd71509983991a0e86e7ae16bffc77197a7a40e6
ratneshchipre <ratneshchipre@gmail.com>
2025-06-09 11:32:04 +0530

    chore: Update API proxy URL in Vite configuration to point to production server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "https://task-manager-app-88x7.onrender.com",
     },
   },
   plugins: [react(), tailwindcss()],
